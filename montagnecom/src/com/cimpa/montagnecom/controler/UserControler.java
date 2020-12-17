a0881bbe8b2cb16954a888b6cf0b2003766ea316
Administrator <Administrator@vmi456564>
2020-12-17 09:40:08 +0100

    test for example (comment deleted)

--- montagnecom/src/com/cimpa/montagnecom/controler/UserControler.java
+++ montagnecom/src/com/cimpa/montagnecom/controler/UserControler.java
@@ -68,7 +68,6 @@ public class UserControler extends HttpServlet {
    *      response)
    */
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
     doGet(request, response);
   }
 
